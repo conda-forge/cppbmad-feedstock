d88c23ddb438cc54fbdeb705d08d60013a143bda
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-02-19 15:40:09 -0800

    BLD: stubs handled upstream

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,20 +4,13 @@ export ACC_ROOT_DIR=$BUILD_PREFIX
 
 export FFLAGS="${FFLAGS} -I${PREFIX}/include/bmad"
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  # We can't create stubs when cross-compiling.
-  # I'll instead make sure this is done upstream in the source repo when tagged.
-  # I might switch this off for all builds, then.
-  STUBGEN=OFF
-else
-  STUBGEN=ON
-fi
-
+# Note: stubs are generated in the upstream repo, we don't need to regenerate
+# them here (I hope).
 cmake -S . -B build ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_PYBMAD=ON \
   -DSKBUILD=OFF \
-  -DSTUBGEN="${STUBGEN}" \
+  -DSTUBGEN=OFF \
   -DPython_EXECUTABLE="$PYTHON" \
   -DVERSION_INFO="${PKG_VERSION}"
 
