dce6c246b0dd919c30194aa4653b29602e0eeaf5
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-02-19 15:01:44 -0800

    BLD: well, maybe not

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,20 @@ export ACC_ROOT_DIR=$BUILD_PREFIX
 
 export FFLAGS="${FFLAGS} -I${PREFIX}/include/bmad"
 
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  # We can't create stubs when cross-compiling.
+  # I'll instead make sure this is done upstream in the source repo when tagged.
+  # I might switch this off for all builds, then.
+  STUBGEN=OFF
+else
+  STUBGEN=ON
+fi
+
 cmake -S . -B build ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_PYBMAD=ON \
   -DSKBUILD=OFF \
+  -DSTUBGEN="${STUBGEN}" \
   -DPython_EXECUTABLE="$PYTHON" \
   -DVERSION_INFO="${PKG_VERSION}"
 
